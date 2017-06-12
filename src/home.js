1e4225f4714595573b4a41056c95f016c4eb29cd
sot528 <sot528@gmail.com>
2017-06-12 14:26:43 +0900

    テンプレートをcarouselに変更

--- src/home.js
+++ src/home.js
@@ -1,11 +1,10 @@
 // CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './lib/cover.css';
+import './lib/carousel.css';
 import './lib/ie10-viewport-bug-workaround.css';
 
 // JS
 import 'bootstrap';
-import './lib/ie8-responsive-file-warning';
 import './lib/ie10-viewport-bug-workaround';
 
 // HTML TODO: ひとまずそのまま。しんどくなったらテンプレートエンジン等導入
