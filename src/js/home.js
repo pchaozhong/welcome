8197b3ba08c95065c780396f5aef41180bb79cd2
sot528 <sot528@gmail.com>
2017-06-15 15:14:32 +0900

    bootstrap-socialを導入

--- src/js/home.js
+++ src/js/home.js
@@ -2,7 +2,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../scss/home.scss';
 import '../lib/ie10-viewport-bug-workaround.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 // JS
 import 'bootstrap';
+import 'bootstrap-social';
 import '../lib/ie10-viewport-bug-workaround';
