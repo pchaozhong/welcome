6a00591ccc240c4aed2134af9c0d8b2ec4feef99
sot528 <sot528@gmail.com>
2017-06-15 18:44:57 +0900

    カルーセル修正: 1枚目のボタンをビデオ再生に変更

--- src/js/home.js
+++ src/js/home.js
@@ -1,8 +1,9 @@
 // CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../scss/home.scss';
-import '../lib/ie10-viewport-bug-workaround.css';
 import 'font-awesome/css/font-awesome.min.css';
+import '../scss/home.scss';
+import '../scss/video-modal.scss';
+import '../lib/ie10-viewport-bug-workaround.css';
 
 // JS
 import 'bootstrap';
