512f05ead351ecac317f59110279a5c3f5ee4cb2
sot528 <sot528@gmail.com>
2017-06-16 15:14:02 +0900

    プラポリ: プラポリページ用資材を追加

---
+++ src/js/privacy.js
@@ -0,0 +1,10 @@
+// CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../lib/ie10-viewport-bug-workaround.css';
+import '../scss/home.scss';
+import '../scss/privacy.scss';
+
+// JS
+import 'bootstrap';
+import 'bootstrap-social';
+import '../lib/ie10-viewport-bug-workaround';
