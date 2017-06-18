5adfdfd25c54ac9ba5d7b92c300c391cac28ea78
sot528 <sot528@gmail.com>
2017-06-19 01:19:39 +0900

    ネタ: コンソール用にネタを仕込む

--- src/js/common.js
+++ src/js/common.js
@@ -26,3 +26,16 @@ $(function () {
     $('.video-modal').modal();
   });
 });
+
+// for console branding.
+(function () {
+  console.log("%c\u2660\u2660\u2660\u2660\u2660\u2660\u2660\u2660\u2660\u2660\u2660\u2660\u2660\u2660\u2660" +
+    "\n%c\u2665\u2665\u2665\u2665\u2665\u2665\u2665\u2665\u2665\u2665\u2665\u2665\u2665\u2665\u2665\n%cALIS" +
+    "\n%cThe decentralized media platform project in Japan.\n%c\u2663\u2663\u2663\u2663\u2663\u2663\u2663\u2663" +
+    "\u2663\u2663\u2663\u2663\u2663\u2663\u2663\n%c\u2666\u2666\u2666\u2666\u2666\u2666\u2666\u2666\u2666\u2666\u2666" +
+    "\u2666\u2666\u2666\u2666", "color: lightblue; font-size: 3em; font-weight: bold;", "color: pink; font-size: 3em;" +
+    " font-weight: bold;", "color: blue; font-size: 10em; font-weight: bold; font-family: american typewriter, " +
+    "brush script mt, harrington, arial, sans-serif;", "color: grey; font-size: 2em; " +
+    "font-family: edwardian script itc, harrington, arial, sans-serif;", "color: lightgreen; font-size: 3em; " +
+    "font-weight: bold", "color: lightgrey; font-size: 3em; font-weight: bold");
+})();
