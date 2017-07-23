0e835a6fa439151bac3f02b7bdfd1388319a2927
fiestared <back.in.fiestared@gmail.com>
2017-07-23 19:57:26 +0900

    clock add

--- src/js/common.js
+++ src/js/common.js
@@ -14,6 +14,11 @@ $(function () {
   });
 });
 
+// カウントダウンの時計
+$('#clock').countdown('2017/09/01', function(event) {
+  $(this).html(event.strftime('%D days %H:%M:%S'));
+});
+
 // カバーのビデオボタンマウスオーバーで色を変更
 $(function () {
   $("#cover-button").mouseover(function () {
