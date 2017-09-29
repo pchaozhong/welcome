50022fdf6477485f2aca3fdc95a7c131b257d226
sot528 <sot528@gmail.com>
2017-09-29 14:36:28 +0900

    remove contribution resources.

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin(),
+    // new webpack.optimize.UglifyJsPlugin(),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
