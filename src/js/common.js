9f1e24e8b40b2211c1eef84baae6bcfe1b53e649
Coco <coco@CoconoMacBook-Pro.local>
2018-02-27 19:09:20 +0900

    fix :: トップ 筐体周りの不具合修正

--- src/js/common.js
+++ src/js/common.js
@@ -107,10 +107,10 @@ $(() => {
     let firePosition = $('[data-scroll]').offset().top + $('[data-scroll]').height() - target.height();
 
     $(window).scroll(function() {
-      if ($(window).scrollTop() + targetOrgTop + 104 > firePosition ) {
+      if ($(window).scrollTop() + targetOrgTop > firePosition ) {
         target.css({'top': firePosition+'px', 'position': 'absolute' });
       }else{
-        target.css({'top': ($(window).scrollTop()/8.5)+targetOrgTop+'px', 'position': 'fixed'});
+        target.css({'top': targetOrgTop+'px', 'position': 'fixed'});
       }
     });
   }
